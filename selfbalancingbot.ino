de68c512100cb3563170ea2af9ab47d6d8a02660
Aditya Kumar <64519341+kumaraditya2002@users.noreply.github.com>
2022-04-24 15:00:59 +0530

    Update selfbalancingbot.ino

--- selfbalancingbot.ino
+++ selfbalancingbot.ino
@@ -1,95 +1,69 @@
 #include <PID_v1.h>
-#include <Wire.h>
+#include <math.h>
 
-long accelX, accelY, accelZ;
-float accAngleY=0;
 
-long gyroX, gyroY, gyroZ;
-float gyroAngleY=0;
+const int x_out = A1;
+const int y_out = A2; 
+const int z_out = A3; 
 
-float prevTime=0,currTime=0,elapsedTime=0;
 
 float pitch;
 double input,output;
-double setpoint = 178;
-double Kp;
-double Kd;
-double Ki;
+double setpoint = 94;
+double Kp=1;
+double Kd=2;
+double Ki=3;
 
 PID pid(&input,&output,&setpoint,Kp,Ki,Kd,DIRECT);
 
 void setup() {
-  Serial.begin(9600);
-  Wire.begin();
-  Wire.beginTransmission(0b1101000); //This is the I2C address of the MPU (b1101000/b1101001 for AC0 low/high datasheet sec. 9.2)
-  Wire.write(0x6B); //Accessing the register 6B - Power Management (Sec. 4.28)
-  Wire.write(0b00000000); //Setting SLEEP register to 0. (Required; see Note on p. 9)
-  Wire.endTransmission();  
-  Wire.beginTransmission(0b1101000); //I2C address of the MPU
-  Wire.write(0x1B); //Accessing the register 1B - Gyroscope Configuration (Sec. 4.4) 
-  Wire.write(0x00000000); //Setting the gyro to full scale +/- 250deg./s 
-  Wire.endTransmission(); 
-  Wire.beginTransmission(0b1101000); //I2C address of the MPU
-  Wire.write(0x1C); //Accessing the register 1C - Acccelerometer Configuration (Sec. 4.5) 
-  Wire.write(0b00000000); //Setting the accel to +/- 2g
-  Wire.endTransmission(); 
-
+  Serial.begin(9600); 
   //pid setup
   pid.SetMode(AUTOMATIC);
   pid.SetSampleTime(10);
   pid.SetOutputLimits(-255,255);
-
-  //motor pin setup
-  pinMode(6,OUTPUT);
-  pinMode(9,OUTPUT);
-  pinMode(10,OUTPUT);
-  pinMode(11,OUTPUT);
-
-  analogWrite(6,0);
-  analogWrite(9,0);
-  analogWrite(10,0);
-  analogWrite(11,0);
 }
 
 void loop() {
-  Wire.beginTransmission(0b1101000); //I2C address of the MPU
-  Wire.write(0x3B); //Starting register for Accel Readings
-  Wire.endTransmission();
-  Wire.requestFrom(0b1101000,6); //Request Accel Registers (3B - 40)
-  while(Wire.available() < 6);
-  accelX = (Wire.read()<<8)|Wire.read()/ 16384; //Store first two bytes into accelX
-  accelY = (Wire.read()<<8)|Wire.read()/ 16384; //Store middle two bytes into accelY
-  accelZ = (Wire.read()<<8)|Wire.read()/ 16384; //Store last two bytes into accelZ
-  accAngleY = atan(-1 * accelX / sqrt(pow(accelY,2) + pow(accelZ,2))) * 180/PI;
+  int x_adc_value, y_adc_value, z_adc_value; 
+  double x_g_value, y_g_value, z_g_value;
+  double roll, pitch, yaw;
+  x_adc_value = analogRead(x_out); /* Digital value of voltage on x_out pin */ 
+  y_adc_value = analogRead(y_out); /* Digital value of voltage on y_out pin */ 
+  z_adc_value = analogRead(z_out); /* Digital value of voltage on z_out pin */ 
   
-  prevTime = currTime;
-  currTime = millis();
-  elapsedTime = (currTime - prevTime) / 1000;
-  Wire.beginTransmission(0b1101000); //I2C address of the MPU
-  Wire.write(0x43); //Starting register for Gyro Readings
-  Wire.endTransmission();
-  Wire.requestFrom(0b1101000,6); //Request Gyro Registers (43 - 48)
-  while(Wire.available() < 6);
-  gyroX = (Wire.read()<<8)|Wire.read()/ 131; //Store first two bytes into accelX
-  gyroY = (Wire.read()<<8)|Wire.read()/ 131; //Store middle two bytes into accelY
-  gyroZ = (Wire.read()<<8)|Wire.read()/ 131; //Store last two bytes into accelZ
-  gyroAngleY = gyroAngleY + gyroY * elapsedTime;
+  
+  x_g_value = ( ( ( (double)(x_adc_value * 5)/1024) - 1.65 ) / 0.330 ); /* Acceleration in x-direction in g units */ 
+  y_g_value = ( ( ( (double)(y_adc_value * 5)/1024) - 1.65 ) / 0.330 ); /* Acceleration in y-direction in g units */ 
+  z_g_value = ( ( ( (double)(z_adc_value * 5)/1024) - 1.80 ) / 0.330 ); /* Acceleration in z-direction in g units */ 
 
-  //complemetry filter
-  pitch = 0.96*gyroAngleY + 0.04*accAngleY;
-  input = pitch + 180;
+  roll = ( ( (atan2(y_g_value,z_g_value) * 180) / 3.14 ) + 180 ); /* Formula for roll */
+  pitch = ( ( (atan2(z_g_value,x_g_value) * 180) / 3.14 ) + 180 ); /* Formula for pitch */
+ 
 
+  Serial.print("Roll = ");
+  Serial.print(roll);
+  Serial.print("\t");
+  Serial.print("Pitch = ");
+  Serial.print(pitch);
+  input=pitch;
   pid.Compute();
-  if(input>150 && input<200) //bot is falling
-  {
-    if(output>0) //falling towards front
-      Forward(); //move forward
-    else if(output<0) //falling towards back
-      Reverse(); //move back
-  }
-  else
-    Stop(); // stop
-  delay(20);
+  Serial.print("Output = ");
+  Serial.print(output);
+  Serial.print("\n\n");
+  
+  
+//  Serial.print(output);
+//  if(input<90 && input>0) //bot is falling
+//  {
+//    if(output>0) //falling towards front
+//      Forward(); //move forward
+//    else if(output<0) //falling towards back
+//      Reverse(); //move back
+//  }
+//  else
+//    Stop(); // stop
+  delay(1000);
 }
 void Forward() //Code to rotate the wheel forward 
 {
